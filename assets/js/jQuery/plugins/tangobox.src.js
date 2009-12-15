c54407c77cc4d010b5e52cf024aab1baebbd603b
Alex Cartwright <alex@tangocms.org>
2009-12-15 19:57:02 +0000

    Implemented Feature #209 and Feature #212

--- assets/js/jQuery/plugins/tangobox.src.js
+++ assets/js/jQuery/plugins/tangobox.src.js
@@ -3,9 +3,7 @@
  * --- jQuery plugin based upon FancyBox by Janis Skarnelis, and ideas taken from jQuery
  * lightbox by Leandro Vieira Pinho.
  *
- * Changes we basically do to Fancybox is remove MSIE6 support, default support for certain
- * selectors, improve the markup/css and major code cleanups. It also handles loading images
- * that the URL does not contain the file extension in.
+ * http://tangocms.org/projects/tangobox
  *
  * @author Alex Cartwright
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU/GPL 2
@@ -18,7 +16,7 @@ jQuery(document).ready(
 		jQuery("a[rel=modalImage]").tangobox();
 	}
 );
- 
+
 (function($) {
 	$.fn.tangobox = function( settings ) {
 		var settings = $.extend(
@@ -32,17 +30,22 @@ jQuery(document).ready(
 									overlayShow:			true,
 									overlayOpacity:			0.7,
 									overlayColor:			'#444',
-									showCloseButton:		true,
+									slideshowAutostart:		false,
+									slideshowDelay:			4500, // Delay of 0 disables slideshow
+									displayMeta:			true, // Display title and controls for images?
 									callbackOnStart:		null,
 									callbackOnShow:			null,
 									callbackOnClose:		null
 								},
 								settings
 							  );
-		var matchedGroup = this; // 'this' refers to the match items passed into the plugin, from the selector.		
-		var elem; // Element the user 'clicked' on.
-		var itemArray = [], itemIndex = 0, busy = false, imageRegExp = /\.(jpe?g|gif|png)($|\?)?$/i;
-		var tbOuter, tbContent; // Store the selectors, saves selecting every time
+		var matchedGroup = this; // 'this' refers to the match items passed into the plugin, from the selector.
+		var elem, // Element the user 'clicked' on.
+			slideshowLength = matchedGroup.length, displayMetaBar,
+			itemArray = [],	itemIndex = 0,
+			busy = false, slideshowId,
+			imageRegExp = /\.(jpe?g|gif|png)($|\?)?$/i,
+			tbOuter, tbContent, tbMeta; // Store the selectors, saves selecting every time
 
 		/**
 		 * Begin the whole process of the Tangobox
@@ -65,7 +68,7 @@ jQuery(document).ready(
 												<a href="" id="tbNavPrev"></a> \
 												<a href="" id="tbNavNext"></a> \
 											</div> \
-											<div id="tbTitle"></div> \
+											<div id="tbMeta"></div> \
 										</div> \
 									</div> \
 								</div>';
@@ -73,6 +76,7 @@ jQuery(document).ready(
 				}
 				tbOuter = $("#tbOuter");
 				tbContent = $("#tbContent");
+				tbMeta = $("#tbMeta");
 				// Update the overlay and change item to the first one.
 				if ( settings.overlayShow ) {
 					$("#tbOverlay").css({
@@ -81,34 +85,81 @@ jQuery(document).ready(
 										}).fadeIn(200);
 				} else {
 					$("tbOverlay").hide();
-				}				
+				}
 				// Use all elements that were matched in the selector
-				var slideShowLength = matchedGroup.length;
-				for( var i = 0; i < slideShowLength; i++ ) {
+				for( var i = 0; i < slideshowLength; i++ ) {
 					itemArray[i] = {
 									elem: matchedGroup[i],
 									href: $(matchedGroup[i]).attr("href"),
 									title: $(matchedGroup[i]).attr("title"),
+									alt: $(matchedGroup[1]).attr("alt"),
 									rel: $(matchedGroup[i]).attr("rel")
 									};
 					if ( itemArray[i].href == $(elem).attr("href") ) {
 						itemIndex = i;
 					}
-				}				
+				}
+				if ( settings.displayMeta ) {
+					// Display if we have more than 1, or a title is to be shown
+					displayMetaBar = itemArray.length > 1 || (itemArray[1].title || itemArray[1].alt);
+				}
 				tbChangeContent();
+				if ( settings.slideshowAutostart ) {
+					tbStartSlideshow();
+				}
 			}
 			return false;
 		};
 
 		/**
+		 * Starts the automatic slideshow using the delay provided. If
+		 * the delay is set to zero, the slideshow wont start.
+		 *
+		 * @return bool
+		 */
+		function tbStartSlideshow() {
+			if ( settings.slideshowDelay <= 0 ) {
+				return false;
+			}
+			slideshowId = setInterval( function() {
+											if ( !busy ) {
+												if ( itemIndex == slideshowLength-1 ) {
+													itemIndex = 0;
+												} else {
+													++itemIndex;
+												}
+												tbChangeContent();
+											}
+									   },
+									   settings.slideshowDelay
+									 );
+			return true;
+		};
+
+		/**
+		 * Stops the current running slideshow (if any)
+		 *
+		 * @return void
+		 */
+		function tbStopSlideshow() {
+			clearInterval( slideshowId );
+			slideshowId = null;
+		}
+
+		/**
 		 * Changes the content that is to be displayed in the main content
 		 * area. The itemIndex of the item which was shown will be returned.
 		 *
-		 * @return int
+		 * @return int|bool
 		 */
 		function tbChangeContent() {
-			$("#tbNavPrev, #tbNavNext, #tbClose, #tbTitle").hide();
+			if ( busy ) {
+				return false;
+			}
+			busy = true;
 			var currentItem = itemArray[ itemIndex ];
+			var cTitle = currentItem.title || currentItem.alt
+			$("#tbNavPrev, #tbNavNext, #tbClose, #tbMeta").hide();
 			if ( tbOuter.is(":visible") === false ) {
 				tbOuter.css("top", "120px").show();
 			}
@@ -116,25 +167,32 @@ jQuery(document).ready(
 			if ( currentItem.href.match(/#/) ) {
 				// Display an element within the content
 				tbOuter.addClass("typeInline");
-				tbSetContent( $(window.location.hash).html(), settings.frameWidth, settings.frameHeight );
+				tbSetContent( $(window.location.hash).html(), cTitle, settings.frameWidth, settings.frameHeight );
 			} else if ( elem.rel == "modalImage" || currentItem.href.match(imageRegExp) ) {
 				// Display image
 				tbOuter.addClass("typeImage");
 				if ( tbContent.is(":visible") ) {
 					tbContent.fadeOut();
 				}
-				var imagePreloader = new Image;
-				imagePreloader.onload = function() {
-											var imgX = imagePreloader.width, imgY = imagePreloader.height;
+				var imgPreload = new Image;
+				imgPreload.onload = function() {
+											var imgX = imgPreload.width, imgY = imgPreload.height;
 											var vpX = $(window).width() - 120, vpY = $(window).height() - 120; // Viewport Height/Width
+											var cTitle = (currentItem.title || currentItem.alt);
+											if ( displayMetaBar ) {
+												// We will be displaying the meta title bar, so shrink image even more.
+												vpY -= tbMeta.outerHeight();
+											}
 											if ( settings.imageScale && (imgX > vpX || imgY > vpY) ) {
 												var ratio = Math.min( Math.min(vpX, imgX) / imgX, Math.min(vpY, imgY) / imgY );
 												imgX = Math.round( ratio * imgX );
 												imgY = Math.round( ratio * imgY );
 											}
-											tbSetContent('<img alt="" id="tbImage" src="'+imagePreloader.src+'" height="'+imgY+'" width="'+imgX+'">', imgX, imgY );
+											tbSetContent( '<img alt="" id="tbImage" src="'+imgPreload.src+'" height="'+imgY+'" width="'+imgX+'">',
+														  cTitle, imgX, imgY
+														);
 										};
-				imagePreloader.src = currentItem.href;
+				imgPreload.src = currentItem.href;
 			} else {
 				// Display 'AJAX' content.
 				tbOuter.addClass("typeAjax");
@@ -145,8 +203,12 @@ jQuery(document).ready(
 											for( var i = 0; i < splitCount; i++ ) {
 												tmpVal = splitQuery[i].split("=");
 												args[ tmpVal[0] ] = tmpVal[1];
-											}									
-											tbSetContent( '<div id="tbInline">'+data+'</div>', (args.width || settings.frameWidth), (args.height || settings.frameHeight) );
+											}
+											tbSetContent( '<div id="tbInline">'+data+'</div>',
+														  cTitle,
+														  (args.width || settings.frameWidth),
+														  (args.height || settings.frameHeight)
+														);
 										 }
 					 );
 			}
@@ -158,42 +220,83 @@ jQuery(document).ready(
 		 *
 		 * @return void
 		 */
-		function tbSetContent( content, cWidth, cHeight ) {			
-			busy = true;
+		function tbSetContent( content, cTitle, cWidth, cHeight ) {
 			cWidth = parseInt( cWidth );
 			cHeight = parseInt( cHeight );
 			// Get viewports height/width to center the content
 			var vpY = $(window).height(), vpScrollY = $(window).scrollTop();
 			var tbPadding = tbOuter.css("paddingTop").replace(/[^\d\.]/g, "") * 2;
 			var cTop = (cHeight + tbPadding) / 2;
-			var tbOuterCssTop = (vpY/2 - cTop)+vpScrollY+"px";		
+			var tbOuterCssTop = (vpY/2 - cTop)+vpScrollY;
 			if ( tbOuter.is(":visible") && cWidth == tbOuter.width() && cHeight == tbOuter.height() ) {
 				var dimSettings = {width: cWidth}; // just to keep jQuery happy, it needs some animation.
-			} else {					
+			} else {
 				var dimSettings = {
 									height: cHeight+"px",
 									width: cWidth+"px"
 								  };
-			}			
+			}
+			// If displaying meta bar, bring tbOuter up by the height of the meta bar.
+			if ( displayMetaBar ) {
+				tbOuterCssTop = (tbOuterCssTop - tbMeta.outerHeight()/2)+"px";
+				tbMeta.html('<p id="tbMetaTitle">'+cTitle+'</p>');
+				if ( itemArray.length > 1 ) {
+					tbMeta.append('<p id="tbMetaControls">Viewing '+(itemIndex+1)+' of '+itemArray.length+' :: </p>');
+					tbMeta.find("p:last").append( $('<a href="" id="tbMetaSlideshow">'+(slideshowId ? "Stop" : "Start")+' slideshow</a>') );
+					tbMeta.find("p:last a").click(function() {
+													if ( slideshowId ) {
+														$(this).html("Start slideshow");
+														$("#tbNav").show();
+														tbStopSlideshow();
+													} else {
+														$(this).html("Stop slideshow");
+														$("#tbNav").hide();
+														tbStartSlideshow();
+													}
+													return false;
+												  });
+				}
+			} else {
+				tbOuterCssTop += "px";
+			}
 			if ( tbOuterCssTop !== tbOuter.css("top") ) {
 				dimSettings.top = tbOuterCssTop;
 				if ( tbOuter.is(":visible") == false && vpScrollY !== 0 && dimSettings.top.replace(/[^\d\.]/g, "") > vpScrollY ) {
 					// Bring the initial tbOuter into the viewport, then resize from there.
 					tbOuter.css("top", vpScrollY+"px");
-				}					
+				}
 			}
 			tbOuter.show();
 			tbContent.fadeOut("normal", function() {
-											tbContent.empty();
-											tbOuter.animate( dimSettings,
-															 settings.zoomSpeedIn,
-															 function() {
-																tbContent.html( $(content) );
-																tbContent.fadeIn("normal", tbFinish);
-															 }
-														);
+											tbContent.empty()
+											tbContent.html( $(content) ).hide();
+											tbOuter.animate( dimSettings, settings.zoomSpeedIn, tbAnimateLoading );
 										}
-								);
+							 );
+		}
+
+		/**
+		 * Does various animations to make a prettyfull transition of loading
+		 * the new content (it has already been set, we just show it now)
+		 *
+		 * @return void
+		 */
+		function tbAnimateLoading() {
+			tbContent.fadeIn("normal", function() {
+											if ( displayMetaBar ) {
+												// Slide in the meta bar
+												tbOuter.animate( {height: tbOuter.height()+tbMeta.outerHeight()+"px"},
+																 settings.zoomSpeedIn,
+																 function() {
+																	tbMeta.show();
+																	tbFinish();
+																 }
+															   );
+											} else {
+												tbFinish();
+											}
+									   }
+							);
 		};
 
 		/**
@@ -220,7 +323,10 @@ jQuery(document).ready(
 			/**
 			 * Setup the navigation (Next/Prev) buttons for images only
 			 */
-			if ( itemArray[ itemIndex ].rel == "modalImage" || itemArray[ itemIndex ].href.match(imageRegExp) ) {
+			if (
+				slideshowId == null &&
+				itemArray[ itemIndex ].rel == "modalImage" || itemArray[ itemIndex ].href.match(imageRegExp)
+			) {
 				if ( itemIndex !== 0 ) {
 					$("#tbNavPrev").show().unbind().click( function() {
 																--itemIndex;
@@ -256,6 +362,7 @@ jQuery(document).ready(
 		 */
 		function tbClose( e ) {
 			if ( e.keyCode === 27 || e.button === 0 ) {
+				tbStopSlideshow();
 				$(document).unbind();
 				tbContent.unbind().empty();
 				tbOuter.css( {height: "250px", width: "250px", top: "0"} ).hide().removeAttr("class");
@@ -267,7 +374,7 @@ jQuery(document).ready(
 		};
 
 		return this.unbind("click").bind("click", _initialize);
-		
+
 	};
 
 })(jQuery);
