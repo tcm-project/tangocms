tcm-project/tangocms

commit fb76dc5f877adb856f47642ff51510c4ed079116

Antispam_Captcha don't log errors. Theme::addCssFile() allows an array
Antispam_Captcha::__construct() no longer logs a warning (or any logs) on failure to remove old captcha files
Theme::addCssFile() first argument can take an array, allowing to add multiple CSS files in one call

application/libraries/Theme.php
@@ -22,7 +22,7 @@
 		 * Version of jQuery to load (used only when use of Google CDN is allowed)
 		 */
 		const _JQUERY_VERSION = 1.3;
-		
+
 		/**
 		 * Name of the current theme
 		 * @var string
@@ -52,13 +52,13 @@
 		 * @var bool
 		 */
 		protected $aggregateJs = false;
-		
+
 		/**
 		 * Toggles if Google's CDN should be used
 		 * @var bool
 		 */
 		protected $googleCdn = false;
-		
+
 		/**
 		 * Stores all loaded JS files from Google's CDN
 		 * @var array
@@ -85,7 +85,7 @@
 			}
 			if ( $this->_config->has( 'cache/google_cdn' ) ) {
 				$this->googleCdn = (bool) $this->_config->get( 'cache/google_cdn' );
-			}		
+			}
 		}
 
 		/**
@@ -306,13 +306,18 @@
 		 * Wrapper for Theme::addHead() to easily add a CSS file from
 		 * CSS assets directory.
 		 *
-		 * @param string $file
-		 * @return bool
+		 * @param string|array $file
+		 * @return int
 		 */
 		public function addCssFile( $file ) {
-			return $this->addHead( 'css',
-								   array('href' => $this->_zula->getDir('assets', true).'/css/'.$file)
-								 );
+			$numAdded = 0;
+			$path = $this->_zula->getDir( 'assets', true );
+			foreach( (array) $file as $css ) {
+				if ( $this->addHead('css', array('href' => $path.'/'.$css)) ) {
+					++$numAdded;
+				}
+			}
+			return $numAdded;
 		}
 
 		/**
@@ -334,8 +339,8 @@
 		 * @param string $module		JS file is a virtual asset for specified module
 		 * @return int|bool				Number of JS files added
 		 */
-		public function addJsFile( $file, $merge=true, $module=null ) {	
-			$numberAdded = 0;		
+		public function addJsFile( $file, $merge=true, $module=null ) {
+			$numberAdded = 0;
 			foreach( (array) $file as $jsFile ) {
 				$isLocal = substr( $jsFile, -3 ) == '.js';
 				if ( !$isLocal && strpos( $jsFile, 'jquery.' ) === 0 ) {
@@ -355,7 +360,7 @@
 				} else {
 					$type = $this->aggregateJs && $merge ? 'merging' : 'standalone';
 				}
-				// Get the right path for the JS file and store it correctly			
+				// Get the right path for the JS file and store it correctly
 				$jsFileDir = $module == null ? $this->_zula->getDir( 'js' ) : Module::getDirectory().'/'.$module.'/assets';
 				$jsPath = $jsFileDir.'/'.$jsFile;
 				if ( _APP_MODE == 'development' ) {
@@ -597,12 +602,12 @@
 							$content = null;
 							foreach( $this->loadedJsFiles[ $type ] as $file ) {
 								$content .= file_get_contents( $file );
-							}	
+							}
 							file_put_contents( $jsFilePath['real'], $content );
 						}
 						$this->addHead( 'js', array('src' => $jsFilePath['html']) );
 					}
-				}				
+				}
 			}
 			$details = array(
 							'page_links'	=> '',

application/libraries/Antispam/Captcha.php
@@ -80,17 +80,12 @@
 			$this->fonts = glob( $this->_zula->getDir( 'fonts' ).'/captcha/*.ttf' );
 			$this->font = $this->fonts[ array_rand( $this->fonts ) ];
 			// Remove old timmed out images
-			$images = glob( $this->tmpDir.'/captcha-*.png' );
-			if ( is_array( $images ) ) {
-				foreach( $images as $file ) {
-					if ( (time() - filemtime( $file )) > $this->timeout ) {
-						$filename = pathinfo( $file, PATHINFO_FILENAME );
-						$captchaId = substr( $filename, strpos($filename, '-') );
-						unset( $_SESSION['antispam']['captcha'][ $captchaId ] );
-						if ( !@unlink( $file ) ) {
-							$this->_log->message( 'Captcha::__construct() could not remove old captcha file', Log::L_WARNING );
-						}
-					}
+			foreach( (array) glob($this->tmpDir.'/captcha-*.png') as $file ) {
+				if ( $file !== false && (time() - filemtime( $file )) > $this->timeout ) {
+					$filename = pathinfo( $file, PATHINFO_FILENAME );
+					$captchaId = substr( $filename, strpos($filename, '-') );
+					unset( $_SESSION['antispam']['captcha'][ $captchaId ] );
+					@unlink( $file );
 				}
 			}
 		}

commit 67da23f42500a2c4a71c3dd945c2aaeca0fbf217

Fixed regression in Theme::addCssFile(), improvements to style of Tangobox
Theme::addCssFile() in commit fb76dc5f877adb856f47642ff51510c4ed079116 caused a regression where it was using wrong path to CSS assets
Tangobox will now re-show the navigation buttons when the slideshow is stopped
Tweaks to 'a' element styles in Tangobox, to ensure it is always the same regardless of theme

application/libraries/Theme.php
@@ -311,7 +311,7 @@
 		 */
 		public function addCssFile( $file ) {
 			$numAdded = 0;
-			$path = $this->_zula->getDir( 'assets', true );
+			$path = $this->_zula->getDir( 'assets', true ).'/css/';
 			foreach( (array) $file as $css ) {
 				if ( $this->addHead('css', array('href' => $path.'/'.$css)) ) {
 					++$numAdded;

assets/css/jquery.tangobox.css
@@ -50,6 +50,18 @@
 	padding: 10px;
 }
 
+#tbInner a[href] {
+	color: #A4260E;
+	outline: none;
+	text-decoration: none;
+	padding-bottom: 1px;
+}
+
+#tbInner a[href]:hover {
+	border-bottom: 1px dashed #A4260E;
+	padding-bottom: 0;
+}
+
 /** Navigation */
 #tbNav > a {
 	padding-bottom: 0;
@@ -91,4 +103,10 @@
 
 #tbMetaTitle {
 	font-weight: bold;
+}
+
+#tbMetaInfo > span {
+	border-left: 2px solid #ddd;
+	margin-left: 5px;
+	padding-left: 5px;
 }

assets/js/jQuery/plugins/tangobox.src.js
@@ -31,7 +31,7 @@ jQuery(document).ready(
 									overlayOpacity:			0.7,
 									overlayColor:			'#444',
 									slideshowAutostart:		false,
-									slideshowDelay:			4500, // Delay of 0 disables slideshow
+									slideshowDelay:			5200, // Delay of 0 disables slideshow
 									displayMeta:			true, // Display title and controls for images?
 									callbackOnStart:		null,
 									callbackOnShow:			null,
@@ -241,20 +241,22 @@ jQuery(document).ready(
 				tbOuterCssTop = (tbOuterCssTop - tbMeta.outerHeight()/2)+"px";
 				tbMeta.html('<p id="tbMetaTitle">'+cTitle+'</p>');
 				if ( itemArray.length > 1 ) {
-					tbMeta.append('<p id="tbMetaControls">Viewing '+(itemIndex+1)+' of '+itemArray.length+' :: </p>');
-					tbMeta.find("p:last").append( $('<a href="" id="tbMetaSlideshow">'+(slideshowId ? "Stop" : "Start")+' slideshow</a>') );
-					tbMeta.find("p:last a").click(function() {
-													if ( slideshowId ) {
-														$(this).html("Start slideshow");
-														$("#tbNav").show();
-														tbStopSlideshow();
-													} else {
-														$(this).html("Stop slideshow");
-														$("#tbNav").hide();
-														tbStartSlideshow();
-													}
-													return false;
-												  });
+					tbMeta.append('<p id="tbMetaInfo">Viewing '+(itemIndex+1)+' of '+itemArray.length+'</p>');
+					if ( settings.slideshowDelay ) {
+						tbMeta.find("p:last").append( $('<span><a href="" id="tbMetaSlideshow">'+(slideshowId ? "Stop" : "Start")+' slideshow</a></span>') );
+						tbMeta.find("p:last a").click(function() {
+														if ( slideshowId ) {
+															$(this).html("Start slideshow");
+															$("#tbNav").show();
+															tbStopSlideshow();
+														} else {
+															$(this).html("Stop slideshow");
+															$("#tbNav").hide();
+															tbStartSlideshow();
+														}
+														return false;
+													});
+					}
 				}
 			} else {
 				tbOuterCssTop += "px";
@@ -323,10 +325,7 @@ jQuery(document).ready(
 			/**
 			 * Setup the navigation (Next/Prev) buttons for images only
 			 */
-			if (
-				slideshowId == null &&
-				itemArray[ itemIndex ].rel == "modalImage" || itemArray[ itemIndex ].href.match(imageRegExp)
-			) {
+			if ( itemArray[ itemIndex ].rel == "modalImage" || itemArray[ itemIndex ].href.match(imageRegExp) ) {
 				if ( itemIndex !== 0 ) {
 					$("#tbNavPrev").show().unbind().click( function() {
 																--itemIndex;
@@ -343,6 +342,9 @@ jQuery(document).ready(
 															}
 														  );
 				}
+			}
+			if ( slideshowId ) {
+				$("#tbNav").hide();
 			}
 			// Different ways of closing the modal box
 			$(document).bind("keyup", tbClose);