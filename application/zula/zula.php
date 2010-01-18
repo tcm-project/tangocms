tcm-project/tangocms

commit 7d4867dd696499b3c71ff059a3e03b30bc228065

Zula + View lib methods return objects, Zula::updateDir() deprecated, Session::destroy() optional arg
Session::destroy() now accepts an optional argument, if provided removes all session auth in database - to effectively logout provided UIDs
View::loadString(), View::allowPhp(), View::caseSensitive(), View::assign(), Zula::loadDefaultLibs(), Zula::setDir(), Zula::updateDir() now return objects ($this)
View::cleanTag(), View::cleanValue(), View::getDefaultTags() visibility is now protected
View::getDefaultTags() no longer caches 'META_KEYWORDS' or 'META_DESCRIPTION' assigned tags
Zula::updateDir() is now deprecated in favour of Zula::setDir() which does the same job
Bumped Zula version to 0.7.70
Removed following exceptions:
	Zula_LibraryNoExist, Zula_FailedDefaultLibs, Zula_FailedSetDirectory, Zula_FailedUpdateDirectory, Zula_DirectoryNoExist
Added following exceptions:
	Zula_Exception

application/zula/zula.php
@@ -13,22 +13,17 @@
  */
 
 	/** Exceptions */
-	class Zula_LibraryNoExist extends Exception {}
- 	class Zula_FailedDefaultLibs extends Exception {}
+	class Zula_Exception extends Exception {}
 
- 	class Zula_FailedSetDirectory extends Exception {}
- 	class Zula_FailedUpdateDirectory extends Exception {}
- 	class Zula_DirectoryNoExist extends Exception {}
-
-	class Zula_ModelNoExist extends Exception {}
-	class Zula_DetailNoExist extends Exception {}
+	class Zula_ModelNoExist extends Zula_Exception {}
+	class Zula_DetailNoExist extends Zula_Exception {}
 
 	final class Zula {
 
 		/**
 		 * Current version of Zula being used
 		 */
-		const _VERSION = '0.7.65';
+		const _VERSION = '0.7.70';
 
 		/**
 		 * Holds the singleton instance of this class
@@ -78,9 +73,7 @@
 		 * The default libraries the the framework will load upon startup
 		 * @var array
 		 */
-		private $defaultLibs = array(
-									'date', 'log', 'locale', 'cache', 'dispatcher', 'error', 'input',
-									);
+		private $defaultLibs = array('date', 'log', 'locale', 'cache', 'dispatcher', 'error', 'input');
 
 		/**
 		 * Path to the main configuration file that Zula will use
@@ -226,36 +219,32 @@
 		/**
 		 * Loads the default most commonly used libraries for the framework
 		 *
-		 * @return bool
+		 * @return object
 		 */
 		public function loadDefaultLibs() {
-			if ( is_array( $this->defaultLibs ) ) {
-				$config = Registry::get( 'config' );
-				foreach( $this->defaultLibs as $library ) {
-					if ( $library == 'cache' ) {
-						try {
-							$cache = Cache::factory( $config->get( 'cache/type' ) );
-						} catch ( Config_KeyNoExist $e ) {
-							// Revert to file based caching.
-							$cache = Cache::factory( 'file' );
-						}
-						try {
-							$cache->ttl( $config->get( 'cache/ttl' ) );
-						} catch ( Exception $e ) {}
-					} else if ( $library == 'locale' ) {
-						try {
-							Locale::factory( $config->get( 'locale/engine' ) );
-						} catch ( Config_KeyNoExist $e ) {
-							Locale::factory( 'failsafe' );
-						}
-					} else {
-						$this->loadLib( $library );
+			$config = Registry::get( 'config' );
+			foreach( $this->defaultLibs as $library ) {
+				if ( $library == 'cache' ) {
+					try {
+						$cache = Cache::factory( $config->get( 'cache/type' ) );
+					} catch ( Config_KeyNoExist $e ) {
+						// Revert to file based caching.
+						$cache = Cache::factory( 'file' );
 					}
+					try {
+						$cache->ttl( $config->get( 'cache/ttl' ) );
+					} catch ( Exception $e ) {}
+				} else if ( $library == 'locale' ) {
+					try {
+						Locale::factory( $config->get( 'locale/engine' ) );
+					} catch ( Config_KeyNoExist $e ) {
+						Locale::factory( 'failsafe' );
+					}
+				} else {
+					$this->loadLib( $library );
 				}
-				return true;
-			} else {
-				throw new Zula_FailedDefaultLibs( 'Zula::loadDefaultLibs() failed to load default libraries, value is not an array', 4 );
 			}
+			return $this;
 		}
 
 		/**
@@ -285,7 +274,7 @@
 				Registry::register( $regName, $tmpLib );
 				return $tmpLib;
 			} else {
-				throw new Zula_LibraryNoExist( 'Zula library "'.$className.'" must extend "Zula_LibraryBase"', 2 );
+				throw new Zula_Exception( 'Zula library "'.$className.'" must extend "Zula_LibraryBase"', 2 );
 			}
 		}
 
@@ -321,49 +310,38 @@
 				$config->load( $configIni );
 				Registry::register( 'config', $config );
 			} catch ( Config_Ini_FileNoExist $e ) {
-				throw new Exception( 'Zula configuration file "'.$configFile.'" does not exist or is not readable', 8);
-			} catch ( Exception $e ) {
-				throw new Exception( 'Zula configuration file could not be loaded "'.$e->getMessage().'"', 8);
+				throw new Zula_Exception( 'Zula configuration file "'.$configFile.'" does not exist or is not readable', 8);
 			}
 			return $config;
 		}
 
 		/**
-		 * Adds a new directory entry that can later be used
+		 * Updates the path to a named directory that can be used. If the path
+		 * does not exist, it shall attemp to create the directory
 		 *
 		 * @param string $name
 		 * @param string $dir
-		 * @return bool
+		 * @return object
 		 */
 		public function setDir( $name, $dir ) {
-			if ( isset( $this->directories[ $name ] ) ) {
-				return true;
-			} else if ( is_dir( $dir ) ) {
-				$this->directories[ $name ] = $dir;
-				return true;
-			} else if ( zula_make_dir( $dir ) === false ) {
-				throw new Zula_FailedSetDirectory( 'Zula was unable create directory "'.$dir.'" due to permissions. Please manually create the directory.', 5 );
-			} else {
-				return true;
+			if ( !is_dir( $dir ) && zula_make_dir( $dir ) === false ) {
+				throw new Zula_Exception( 'failed to create directory "'.$dir.'"' );
 			}
+			$this->directories[ $name ] = $dir;
+			unset( $this->htmlDirs[ $name ] );
+			return $this;
 		}
 
 		/**
-		 * Updates a stored directories directory, if given a new name
-		 * it will attempt to set it via set_dir().
+		 * Alias to Zula::setDir()
 		 *
 		 * @param string $name
 		 * @param string $dir
-		 * @return bool
+		 * @deprecated deprecated since 0.7.70
+		 * @return object
 		 */
 		public function updateDir( $name, $dir ) {
-			if ( isset( $this->htmlDirs[ $name ] ) ) {
-				unset( $this->htmlDirs[ $name ] );
-			}
-			if ( !isset( $this->directories[ $name ] ) ) {
-				return $this->setDir( $name, $dir );
-			}
-			return $this->directories[ $name ] = $dir;
+			return $this->setDir( $name, $dir );
 		}
 
 		/**
@@ -394,7 +372,7 @@
 				}
 				return rtrim( $this->directories[ $name ], '/' );
 			}
-			throw new Zula_DirectoryNoExist( 'Zula could not get directory "'.$name.'" as it does not exist', 6 );
+			throw new Zula_Exception( 'Zula named directory "'.$name.'" does not exist' );
 		}
 
 		/**

index.php
@@ -49,23 +49,23 @@
 
 	/**
 	 * Default directories in Zula should be fine, though you can configure
-	 * them from here if you want, via $zula->updateDir(); EG:
-	 * $zula->updateDir( 'lib', '/libaries' );
+	 * them from here if you want, via $zula->setDir(); EG:
+	 * $zula->setDir( 'lib', '/libaries' );
 	 */
-	$zula->updateDir( 'config', dirname(__FILE__).'/config' );
-	$zula->updateDir( 'uploads', './assets/uploads' );
+	$zula->setDir( 'config', dirname(__FILE__).'/config' );
+	$zula->setDir( 'uploads', './assets/uploads' );
 
 	if ( _APP_MODE == 'installation' ) {
 		define( '_REAL_MODULE_DIR', $zula->getDir( 'modules' ) );
 		// Reconfigure some directories so they work correctly when installing
-		$zula->updateDir( 'modules', './modules' );
-		$zula->updateDir( 'themes', './themes' );
-		$zula->updateDir( 'assets', '../assets' );
-		$zula->updateDir( 'js', '../assets/js' );
-		$zula->updateDir( 'tmp', '../tmp' );
-		$zula->updateDir( 'install', './' );
-		$zula->updateDir( 'uploads', '../assets/uploads' );
-		$zula->updateDir( 'config', '../config' );
+		$zula->setDir( 'modules', './modules' );
+		$zula->setDir( 'themes', './themes' );
+		$zula->setDir( 'assets', '../assets' );
+		$zula->setDir( 'js', '../assets/js' );
+		$zula->setDir( 'tmp', '../tmp' );
+		$zula->setDir( 'install', './' );
+		$zula->setDir( 'uploads', '../assets/uploads' );
+		$zula->setDir( 'config', '../config' );
 	}
 
 	Registry::register( 'zula', $zula );
@@ -88,7 +88,7 @@
 			$serverName .= _BASE_DIR;
 		}
 		$serverName = str_replace( '/', '.', rtrim($serverName, '/') );
-		if ( strlen( $serverName ) ) {			
+		if ( strlen( $serverName ) ) {
 			if ( substr( $serverName, -8 ) == '.install' ) {
 				$serverName = substr( $serverName, 0, -8 );
 			}
@@ -97,14 +97,14 @@
 			}
 		}
 	}
-	$zula->updateDir( 'config', $confDir );
+	$zula->setDir( 'config', $confDir );
 	// Load the main configuration file for the project and define version
 	$config = $zula->loadMainConfig( $zula->getDir( 'config' ).'/config.ini.php' );
 	define( '_PROJECT_VERSION', $config->get( 'config/version' ) );
 
 	/**
 	 * Load the default libraries that are most commonly needed if you wish to
-	 * load more, then simply use $zula->load_lib()
+	 * load more, then simply use $zula->loadLib()
 	 */
 	$zula->loadDefaultLibs();
 	Module::setDirectory( $zula->getDir( 'modules' ) );

application/libraries/Session.php
@@ -128,8 +128,8 @@
 		}
 
 		/**
-		 * Checks if a user is 'logged in'. A user 'logged in' is basically
-		 * anyone not a guest.
+		 * Checks if a user is 'logged in'. Returns true if the current user
+		 * identified is not a guest.
 		 *
 		 * @return bool
 		 */
@@ -217,28 +217,43 @@
 		}
 
 		/**
-		 * Completely destroys a session and all user/group info
-		 * a long with it, including all authentication/session
-		 * cookies.
+		 * If no uid is provided, then the complete session will be destroyed
+		 * for the current user - including all authentication/session cookies
+		 * and shall be identified as 'guest' afterwards.
 		 *
-		 * User shall be identified as 'guest' afterwards.
+		 * Providing a uid shall clear all stored session authentication data
+		 * within the database, effectively logging that user out from all
+		 * locations. An int representing number of sessions closed is returned.
 		 *
-		 * @return bool
+		 * @param int|array $uid
+		 * @return bool|int
 		 */
-		public function destroy() {
-			if ( isset($_SESSION['auth']['key']) ) {
-				$pdoSt = $this->_sql->prepare( 'DELETE FROM {SQL_PREFIX}sessions WHERE session_key = ?' );
-				$pdoSt->execute( array($_SESSION['auth']['key']) );
+		public function destroy( $uid=null ) {
+			if ( $uid ) {
+				$pdoSt = $this->_sql->prepare( 'DELETE FROM {SQL_PREFIX}sessions WHERE uid = :uid' );
+				$sessionsClosed = 0;
+				foreach( (array) $uid as $user ) {
+					$pdoSt->bindValue( ':uid', $user, PDO::PARAM_INT );
+					$sessionsClosed += $pdoSt->rowCount();
+				}
 				$pdoSt->closeCursor();
+				return $sessionsClosed;
+			} else {
+				if ( isset($_SESSION['auth']['key']) ) {
+					$pdoSt = $this->_sql->prepare( 'DELETE FROM {SQL_PREFIX}sessions WHERE session_key = ?' );
+					$pdoSt->execute( array($_SESSION['auth']['key']) );
+					$pdoSt->closeCursor();
+				}
+				$_SESSION = array();
+				foreach( array('zulaAuthKey', 'zulaAuthFor', session_name()) as $cookie ) {
+					setcookie( $cookie, '', time()-42000, _BASE_DIR );
+				}
+				session_regenerate_id( true );
+				session_destroy();
+				$this->user = $this->group = array();
+				$this->identify();
+				return true;
 			}
-			$_SESSION = array();
-			foreach( array('zulaAuthKey', 'zulaAuthFor', session_name()) as $cookie ) {
-				setcookie( $cookie, '', time()-42000, _BASE_DIR );
-			}
-			session_regenerate_id( true );
-			session_destroy();
-			$this->identify();
-			return true;
 		}
 
 		/**

application/libraries/Theme.php
@@ -684,7 +684,9 @@
 		 * @return string
 		 */
 		public function makePageLinks( array $links ) {
-			if ( !empty( $links ) ) {
+			if ( empty( $links ) ) {
+				return '';
+			} else {
 				$linkItems = array();
 				foreach( $links as $title=>$url ) {
 					$url = !trim( $url ) ? '#' : zula_htmlspecialchars( $url );
@@ -693,8 +695,6 @@
 					$linkItems[] = '<li><a href="'.$url.'" title="'.$title.'">'.$title.'</a></li>';
 				}
 				return '<ul id="pagelinks">'.implode( '', $linkItems ).'</ul>';
-			} else {
-				return '';
 			}
 		}
 

application/libraries/View.php
@@ -104,15 +104,33 @@
 		 * Please note, any PHP within the content will *not* be executed.
 		 *
 		 * @param string $content
-		 * @return bool
+		 * @return object
 		 */
 		public function loadString( $content ) {
-			if ( is_string( $content ) ) {
-				$this->loadedContent = $content;
-				return true;
-			} else {
-				return false;
-			}
+			$this->loadedContent = (string) $content;
+			return $this;
+		}
+
+		/**
+		 * Sets if PHP should be parsed/allowed in the view file
+		 *
+		 * @param bool $allow
+		 * @return object
+		 */
+		public function allowPhp( $allow=true ) {
+			$this->parsePhp = (bool) $allow;
+			return $this;
+		}
+
+		/**
+		 * Sets if this views tags should be case-sensitive
+		 *
+		 * @param bool $cs
+		 * @return ojbect
+		 */
+		public function caseSensitive( $cs=true ) {
+			$this->caseSensitive = (bool) $cs;
+			return $this;
 		}
 
 		/**
@@ -125,25 +143,24 @@
 		}
 
 		/**
-		 * Sets if PHP should be parsed/allowed in the view file
+		 * Checks if a tag has been assigned to this view file
 		 *
-		 * @param bool $allow
 		 * @return bool
 		 */
-		public function allowPhp( $allow=true ) {
-			$this->parsePhp = (bool) $allow;
-			return true;
-		}
-
-		/**
-		 * Sets if this views tags should be case-sensitive
-		 *
-		 * @param bool $cs
-		 * @return bool
-		 */
-		public function caseSensitive( $cs=true ) {
-			$this->caseSensitive = (bool) $cs;
-			return true;
+		public function isAssigned( $tag ) {
+			$tagTokens = explode( '.', $tag );
+			if ( count( $tagTokens ) <= 1 ) {
+				$tmpTag = $tag;
+			} else {
+				// Tag is in the format of {FOO.BAR.CAR}
+				$tmpTag = $tagTokens;
+			}
+			try {
+				$this->replaceTag( $tmpTag, '' );
+				return true;
+			} catch ( Exception $e ) {
+				return false;
+			}
 		}
 
 		/**
@@ -154,7 +171,7 @@
 		 * @param bool $overwrite If a tag already exists, should it be overwriten or appended?
 		 * @param bool $allowHtml should HTML be allowed in the tag value?
 		 * @param bool $prepend	Prepend content to the tag, instead of append
-		 * @return bool
+		 * @return object
 		 */
 		public function assign( array $tags, $overwrite=true, $allowHtml=false, $prepend=false ) {
 			if ( $this->caseSensitive === false ) {
@@ -169,7 +186,6 @@
 			}
 			if ( empty( $this->assignedTags ) ) {
 				$this->assignedTags = $tags;
-				return true;
 			} else if ( $overwrite == false ) {
 				/**
 				 * Allows tags that have been assigned more than once to be
@@ -194,11 +210,10 @@
 						$this->assignedTags[ $key ] = $val;
 					}
 				}
-				return true;
 			} else {
 				$this->assignedTags = zula_merge_recursive( $this->assignedTags, $tags );
-				return true;
 			}
+			return $this;
 		}
 
 		/**
@@ -207,7 +222,7 @@
 		 * @param array $tags
 		 * @param bool $overwrite If a tag already exists, should it be overwriten or appended?
 		 * @param bool $prepend	Prepend content to the tag, instead of append
-		 * @return bool
+		 * @return object
 		 */
 		public function assignHtml( $tags, $overwrite=true, $prepend=false ) {
 			return $this->assign( $tags, $overwrite, true, $prepend );
@@ -219,7 +234,7 @@
 		 * @param string $tag
 		 * @return string
 		 */
-		public function cleanTag( $tag ) {
+		protected function cleanTag( $tag ) {
 			return preg_replace( '#[^A-Z0-9_\-/ ]#i', '', $tag );
 		}
 
@@ -229,7 +244,7 @@
 		 * @param mixed $value
 		 * @return mixed
 		 */
-		public function cleanTagValue( $value ) {
+		protected function cleanTagValue( $value ) {
 			if ( is_array( $value ) ) {
 				foreach( $value as $key=>$val ) {
 					if ( is_array( $val ) ) {
@@ -243,27 +258,6 @@
 				return htmlspecialchars( $value, ENT_COMPAT, 'UTF-8' );
 			} else {
 				return $value;
-			}
-		}
-
-		/**
-		 * Checks if a tag has been assigned to this view file
-		 *
-		 * @return bool
-		 */
-		public function isAssigned( $tag ) {
-			$tagTokens = explode( '.', $tag );
-			if ( count( $tagTokens ) <= 1 ) {
-				$tmpTag = $tag;
-			} else {
-				// Tag is in the format of {FOO.BAR.CAR}
-				$tmpTag = $tagTokens;
-			}
-			try {
-				$this->replaceTag( $tmpTag, '' );
-				return true;
-			} catch ( Exception $e ) {
-				return false;
 			}
 		}
 
@@ -470,7 +464,7 @@
 		 *
 		 * @return array
 		 */
-		public function getDefaultTags() {
+		protected function getDefaultTags() {
 			if ( !$tags = $this->_cache->get( 'view_default_tags' ) ) {
 				try {
 					$tmpLang = explode( '.', $this->_config->get( 'locale/default' ) );
@@ -485,8 +479,6 @@
 								'DIR_JAVASCRIPT'		=> $this->_zula->getDir( 'js', true ),
 								'DIR_THEME'				=> $this->_zula->getDir( 'themes', true ),
 								'DIR_UPLOADS'			=> $this->_zula->getDir( 'uploads', true ),
-								'META_DESCRIPTION'		=> $this->_config->get( 'meta/description' ),
-								'META_KEYWORDS'			=> $this->_config->get( 'meta/keywords' ),
 								'SITE_SLOGAN'			=> $this->_config->get( 'config/slogan' ),
 								'SITE_TITLE'			=> $this->_config->get( 'config/title' ),
 								'URL_ADMIN'				=> $this->_router->makeUrl( null, null, null, 'admin' ),
@@ -498,13 +490,13 @@
 				$tags['plain'] = array(
 										'SITE_TITLE'		=> $tags['SITE_TITLE'],
 										'SITE_SLOGAN'		=> $tags['SITE_SLOGAN'],
-										'META_KEYWORDS'		=> $tags['META_KEYWORDS'],
-										'META_DESCRIPTION'	=> $tags['META_DESCRIPTION'],
 										);
 				$this->_cache->add( 'view_default_tags', $tags );
 			}
 			// Add in some tags which should not be cached
 			$tags['URL_CURRENT_ST'] = $this->_router->makeUrl( '' );
+			$tags['META_DESCRIPTION'] = $this->_config->get( 'meta/description' );
+			$tags['META_KEYWORDS'] = $this->_config->get( 'meta/keywords' );
 			if ( Registry::has( 'theme' ) ) {
 				$curTheme = Registry::get( 'theme' )->getDetail( 'name' );
 				$tags['DIR_CUR_THEME'] = $tags['DIR_THEME'].'/'.$curTheme;

commit 197aaacb6adf7f68b171d9bd2960ee36c3f3d306

Fixed regression in 7d4867dd696499b3c71ff059a3e03b30bc228065 where it was creating directories
Removed some Zula directories that were being tracked, but never used
build.sh now has optional path to the Google Closure JAR file, and 'p' argument is now optional, defaults to 'production'

application/zula/zula.php
@@ -53,9 +53,7 @@
 									'libs'		=> '/libraries',
 									'locale'	=> '/locale',
 									'logs'		=> '/logs',
-									'models'	=> '/models',
 									'modules'	=> '/modules',
-									'plugin'	=> '/plugins',
 									'themes'	=> './assets/themes',
 									'tmp'		=> './tmp',
 									'uploads'	=> './assets/uploads',
@@ -317,14 +315,16 @@
 
 		/**
 		 * Updates the path to a named directory that can be used. If the path
-		 * does not exist, it shall attemp to create the directory
+		 * does not exist, it shall attemp to create the directory (only if the
+		 * 3rd argument is set to bool true)
 		 *
 		 * @param string $name
 		 * @param string $dir
+		 * @param bool $createDir
 		 * @return object
 		 */
-		public function setDir( $name, $dir ) {
-			if ( !is_dir( $dir ) && zula_make_dir( $dir ) === false ) {
+		public function setDir( $name, $dir, $createDir=false ) {
+			if ( $createDir && zula_make_dir( $dir ) === false ) {
 				throw new Zula_Exception( 'failed to create directory "'.$dir.'"' );
 			}
 			$this->directories[ $name ] = $dir;
@@ -337,11 +337,12 @@
 		 *
 		 * @param string $name
 		 * @param string $dir
+		 * @param bool $createDir
 		 * @deprecated deprecated since 0.7.70
 		 * @return object
 		 */
-		public function updateDir( $name, $dir ) {
-			return $this->setDir( $name, $dir );
+		public function updateDir( $name, $dir, $createDir=false ) {
+			return $this->setDir( $name, $dir, $createDir );
 		}
 
 		/**

index.php
@@ -53,7 +53,6 @@
 	 * $zula->setDir( 'lib', '/libaries' );
 	 */
 	$zula->setDir( 'config', dirname(__FILE__).'/config' );
-	$zula->setDir( 'uploads', './assets/uploads' );
 
 	if ( _APP_MODE == 'installation' ) {
 		define( '_REAL_MODULE_DIR', $zula->getDir( 'modules' ) );

utils/build.sh
@@ -10,9 +10,9 @@
 #---
 
 ## All options and tasks with their default values
-OPT_PATH_CLOSURE=/usr/bin/closure
+OPT_PATH_CLOSURE=/usr/share/java/closure-compiler/closure-compiler.jar
 OPT_VERBOSE=false
-OPT_PACKAGE_MODE=
+OPT_PACKAGE_MODE=production
 
 TASK_JS_COMPRESS=false
 TASK_PACKAGE=false
@@ -23,39 +23,47 @@ if [ ! -f "index.php" ]; then
 	exit 1
 fi
 
-while getopts ":jp:vxh" OPTION
-do
-	case "$OPTION" in
-		j)
+while [[ $1 == -* ]]; do
+	case "$1" in
+		-j)
 			TASK_JS_COMPRESS=true
-			if [[ -n "$OPTARG" ]]; then
-				OPT_PATH_CLOSURE="$OPTARG"
+			if (($# > 1)); then
+				OPT_PATH_CLOSURE="$2"
+				shift 2;
+			else
+				shift
 			fi
 			;;
-		p)
+		-p)
 			TASK_PACKAGE=true
-			OPT_PACKAGE_MODE="$OPTARG"
+			if (($# > 1)); then
+				OPT_PACKAGE_MODE="$2"
+				shift 2;
+			fi
 			;;
-		v)
+		-v)
 			OPT_VERBOSE=true
+			shift
 			;;
-		x)
+		-x)
 			TASK_CHECK_EXCEPTIONS=true
+			shift
 			;;
-		h)
-			echo -e "TangoCMS Project building tools.\nUsage: build.sh [-jpvxh]"
+		-h)
+			echo -e "TangoCMS Project building tools.\nUsage: build.sh [-j [jar-path]] [-p [mode]] [-x] [-v] [-h]"
 			echo "Options:"
 			echo -e "\t-j\tCompress source JavaScript files using Google Closure Compiler."
 			echo -e "\t-p\tCreates .tar.gz, .tar.bz2 and .zip archives. This implies '-j' always. Zula" \
-					"\n\t\tapplication mode argument required, either 'development' or 'production.'"
+					"\n\t\tapplication mode argument optional, either 'development' or 'production which'" \
+					"\n\t\tdefaults to 'production'."
 			echo -e "\t-x\tCheck all thrown PHP exceptions are defined, and list those not used."
 			echo -e "\t-v\tBe more verbose with output, providing more detail."
 			echo -e "\t-h\tDisplays this help text.\n"
 			echo "Report bugs to <bugs@tangocms.org>"
 			exit 0
 			;;
-		*)
-			echo "Invalid argument or missing value for '$OPTARG'. See '-h' for help text."
+		-*)
+			echo "Invalid argument or missing value for '$1'. See '-h' for help text."
 			exit 1
 			;;
 	esac
@@ -75,8 +83,8 @@ if [ $TASK_JS_COMPRESS == "true" -o $TASK_PACKAGE == "true" ]; then
 	verbose || echo -ne ":: Compressing source JavaScript files (*.src.js) "
 	for sourceFile in `find . -name "*.src.js"`; do
 		fileName=`basename $sourceFile .src.js`.js
-		$OPT_PATH_CLOSURE --js $sourceFile --charset utf-8 --js_output_file `dirname $sourceFile`/$fileName \
-						  --warning_level QUIET
+		"$JAVA_HOME/bin/java" -jar $OPT_PATH_CLOSURE --js $sourceFile --charset utf-8 --js_output_file `dirname $sourceFile`/$fileName \
+							  --warning_level QUIET
 		verbose || echo -ne "."
 	done
 	verbose || echo " done!"