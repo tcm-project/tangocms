30d45c66af8378bc1173e47ac959cc8d122a3f14
Alex Cartwright <acartwright@mutinydesign.co.uk>
2010-04-06 08:38:27 +0100

    Set version numbers to 2.5.3, added upgrade routes and changelog

--- install/index.php
+++ install/index.php
@@ -20,7 +20,7 @@
 	define( '_PATH_APPLICATION', '../application' );
 	define( '_APP_MODE', 'installation' );
 
-	define( '_PROJECT_LATEST_VERSION', '2.5.2' );
+	define( '_PROJECT_LATEST_VERSION', '2.5.3' );
 
 	// Include the main index.php file
 	require_once '../index.php';
