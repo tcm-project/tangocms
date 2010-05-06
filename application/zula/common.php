ca8f7c7288f1fdcdce607a9a6391e8299db366b3
Alex Cartwright <acartwright@mutinydesign.co.uk>
2010-05-06 13:18:26 +0100

    Fixed Internal Issue #273
    - Fixed Internal Issue #273 "zula_htmlspecialchars() use ENT_QUOTES instead"
    - View::cleanTagValue() now uses zula_htmlspecialchars() instead of
      htmlspecialchars()

--- application/zula/common.php
+++ application/zula/common.php
@@ -630,7 +630,7 @@ ERR;
 	 * @return string
 	 */
 	function zula_htmlspecialchars( $string ) {
-		return htmlspecialchars( htmlspecialchars_decode($string), ENT_COMPAT, 'UTF-8' );
+		return htmlspecialchars( htmlspecialchars_decode($string, ENT_QUOTES), ENT_QUOTES, 'UTF-8' );
 	}
 
 	/**
