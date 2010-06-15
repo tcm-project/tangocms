85a27208ae49ce6d0e259ace9a793467659ff12d
Alex Cartwright <alex@tangocms.org>
2010-06-15 17:33:18 +0100

    Set version numbers to 2.5.5. and updated NEWS file

--- install/index.php
+++ install/index.php
@@ -20,7 +20,7 @@
 	define( '_PATH_APPLICATION', '../application' );
 	define( '_APP_MODE', 'installation' );
 
-	define( '_PROJECT_LATEST_VERSION', '2.5.4' );
+	define( '_PROJECT_LATEST_VERSION', '2.5.5' );
 
 	// Include the main index.php file
 	require_once '../index.php';
