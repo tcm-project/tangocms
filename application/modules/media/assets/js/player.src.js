1e48df313444ff94886bb4301fab4e4cb7db2324
Alex Cartwright <alex@tangocms.org>
2010-08-18 20:31:03 +0100

    Upgraded to Flowplayer 3.2.3
    This allows us to remove the slight hacky way of forcing the audio pluging to
    load, and shortened the code.

--- application/modules/media/assets/js/player.src.js
+++ application/modules/media/assets/js/player.src.js
@@ -1,11 +1,10 @@
-
 /*!
  * TangoCMS Media
  *
  * @patches submit all patches to patches@tangocms.org
  *
  * @author Alex Cartwright
- * @copyright Copyright (C) 2008 2009, 2010 Alex Cartwright
+ * @copyright Copyright (C) 2008, 2009, 2010 Alex Cartwright
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU/GPL 2
  * @package TangoCMS_Media
  */
@@ -14,29 +13,18 @@ $(document).ready(
 	function() {
 		var defaultOpts = {
 							canvas: {backgroundColor: "#0C1013", backgroundGradient: "low", border: "1px solid #000", borderRadius: "10"},
-							clip: {autoPlay: true}
+							clip: {autoPlay: true, scaling: "fit"}
 						  }
 		$("a.mediaPlayer").each( function() {
 			if ( $(this).hasClass("audio") ) {
 				var playerOpts = {
 									plugins: {
-										controls: {autoHide: false},
-										audio: {url: zula_dir_js+"/flowplayer/flowplayer.audio.swf"}
+										controls: {autoHide: false}
 									},
-									playlist: [{url: $(this).attr("href"), provider: "audio"}]
-								}
-			} else {
-				var playerOpts = {
-									plugins: {
-										pseudo: {
-											url: zula_dir_js+"/flowplayer/flowplayer.pseudostreaming.swf",
-											queryString: escape("&start=${start}")
-										}
-									},
-									playlist: [{url: $(this).attr("href"), provider: "pseudo", scaling: "fit"}]
+									clip: {type: "audio"}
 								}
 			}
-			$(this).flowplayer( {src: zula_dir_js+"/flowplayer/flowplayer.swf", wmode: "transparent"},
+			$(this).flowplayer( {src: zula_dir_js+"/flowplayer/flowplayer-3.2.3.swf", wmode: "transparent"},
 								$.extend(defaultOpts, playerOpts) );
 		});
 	}
