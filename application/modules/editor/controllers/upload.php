c25b717b2bf5949602ef4d84d16650121e866452
Alex Cartwright <alex@tangocms.org>
2010-09-26 16:41:43 +0100

    Fixed some Zula compatibility issues and improved max size error code

--- application/modules/editor/controllers/upload.php
+++ application/modules/editor/controllers/upload.php
@@ -19,9 +19,6 @@
 		 * @return string
 		 */
 		public function indexSection() {
-			$this->_locale->textDomain( $this->textDomain() );
-			$this->_dispatcher->standalone();
-			// Handle uploading of the file
 			try {
 				$uploader = new Uploader( 'upload' );
 				$uploader->uploadDir( $this->_zula->getDir('uploads').'/editor/' )
@@ -36,30 +33,20 @@
 			} catch ( Uploader_NotEnabled $e ) {
 				$error = t('Sorry, it appears file uploads are disabled within your PHP configuration');
 			} catch ( Uploader_MaxFileSize $e ) {
-				$maxSize = zula_byte_value( ini_get('upload_max_filesize') );
-				if ( $maxSize >= pow(1024, 3) ) {
-					$suffix = 'GiB';
-					$maxSize /= pow(1024, 3);
-				} else if ( $maxSize >= pow(1024, 2) ) {
-					$suffix = 'MiB';
-					$maxSize /= pow(1024, 2);
-				} else if ( $maxSize >= 1024 ) {
-					$suffix = 'KiB';
-					$maxSize /= 1024;
-				} else {
-					$suffix = 'bytes';
-				}
-				$error = sprintf( t('Selected file exceeds the maximum allowed file size of %s'), number_format($maxSize).$suffix );
+				$error = sprintf( t('Selected file exceeds the maximum allowed file size of %s'),
+									zula_human_readable($e->getMessage())
+							   );
 			} catch ( Uploader_Exception $e ) {
 				$error = t('Oops, an error occurred while uploading your files');
 			}
 			// Return the HTML response that CKEditor requires
 			$cb = $this->_input->get( 'CKEditorFuncNum' );
-			return '<html><body>
+			echo '<html><body>
 						<script type="text/javascript">
 							window.parent.CKEDITOR.tools.callFunction('.$cb.', "'.$file->path.'", "'.$error.'");
 						</script>
 					</body></html>';
+			return true;
 		}
 
 	}
