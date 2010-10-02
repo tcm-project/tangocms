a2a5e9be48e6cacf473fc4d83360dcb4ceba036d
Alex Cartwright <alex@tangocms.org>
2010-10-02 21:07:14 +0100

    2.5.64 released; upgrade routes for 72e1fa8 (feature #316)

--- config/default.dist/config.ini.php
+++ config/default.dist/config.ini.php
@@ -138,7 +138,7 @@ default = en_US.UTF-8
 title = "Powered By TangoCMS"
 title_format = "[PAGE] | [SITE_TITLE]"
 slogan = "Powered by TangoCMS"
-version = 2.5.63
+version = 2.5.64
 
 ;---
 ; Meta Data
--- install/index.php
+++ install/index.php
@@ -13,7 +13,7 @@
  * @package Zula_Installer
  */
 
-	define( '_PROJECT_LATEST_VERSION', '2.5.63' );
+	define( '_PROJECT_LATEST_VERSION', '2.5.64' );
 
 	$state = 'installation';
 	require '../index.php';
