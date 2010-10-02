tcm-project/tangocms

commit 8d2b77d253c6a689ddae83ef730cb9e2166a3ae6

Acl::check() returns bool false if resource does not exist, no exception thrown

application/libraries/Acl.php
@@ -427,10 +427,11 @@
     	}
 
 		/**
-		 * Checks if a specified role is allowed to access resource
+		 * Checks if a specified role is allowed to access resource. If the
+		 * resource does not exist, bool false will be returned.
 		 *
 		 * The role is checked starting at the specified role and for every
-		 * parent of that role. IF any of the roles parents have access
+		 * parent of that role. If any of the roles parents have access
 		 * to the resource, the role will to.
 		 *
 		 * @param string|int $resource
@@ -439,7 +440,11 @@
 		 * @return bool
 		 */
 		public function check( $resource, $role='', $allowRoot=true ) {
-			$resource = $this->getResource( $resource );
+			try {
+				$resource = $this->getResource( $resource );
+			} catch ( Acl_ResourceNoExist $e ) {
+				return false;
+			}
       		if ( empty( $resource ) ) {
       			return false;
       		} else if ( !trim( $role ) ) {
@@ -561,9 +566,9 @@
 					try {
 						$role['access'] = (bool) $this->_input->post( 'acl_resources/'.$tmpResource.'/'.$role['name'] );
 					} catch ( Input_KeyNoExist $e ) {
-						try {
-							$role['access'] = $this->check( $tmpResource, $role['name'], false );
-						} catch ( Acl_ResourceNoExist $e ) {
+						if ( $this->check( $tmpResource, $role['name'], false ) ) {
+							$role['access'] = true;
+						} else {
 							$role['access'] = in_array( $role['id'], (isset($tmpRoleHint) ? $tmpRoleHint : $roleHint) );
 						}
 					}

application/libraries/Module.php
@@ -547,12 +547,8 @@
 				throw new Module_Disabled( $lMsg );
 			} else if ( _ACL_ENABLED ) {
 				$resource = $this->name.'_global';
-				try {
-					if ( !$this->_acl->check( $resource ) ) {
-						throw new Module_NoPermission( $resource );
-					}
-				} catch ( ACL_ResourceNoExist $e ) {
-					throw new Module_NoExist( 'missing ACL resource "'.$resource.'"' );
+				if ( !$this->_acl->check( $resource ) ) {
+					throw new Module_NoPermission( $resource );
 				}
 			}
 			if ( !$this->controllerExists( $cntrlr ) ) {

commit 2f4077dcae35a3874a70f54edb975a39287d6783

Fixed regression in 8d2b77d, causing ACL form displayed incorrect permissions
When a resource existed, it behaved as if it didn't and set the permissions to
be checked by default of those of the 'group_guest' roll tree. Much like it does
when the resource does not exist

application/libraries/Acl.php
@@ -566,8 +566,8 @@
 					try {
 						$role['access'] = (bool) $this->_input->post( 'acl_resources/'.$tmpResource.'/'.$role['name'] );
 					} catch ( Input_KeyNoExist $e ) {
-						if ( $this->check( $tmpResource, $role['name'], false ) ) {
-							$role['access'] = true;
+						if ( $this->resourceExists( $tmpResource ) ) {
+							$role['access'] = $this->check( $tmpResource, $role['name'], false );
 						} else {
 							$role['access'] = in_array( $role['id'], (isset($tmpRoleHint) ? $tmpRoleHint : $roleHint) );
 						}