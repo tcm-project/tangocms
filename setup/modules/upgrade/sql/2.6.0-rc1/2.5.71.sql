048ba1653e9359b96390acd88909f520b1755bbc
Alex Cartwright <alex@tangocms.org>
2010-11-07 20:55:55 +0000

    Fixed bug in 2.5.71 upgrade SQL file, incorrect column trying to change

--- setup/modules/upgrade/sql/2.6.0-rc1/2.5.71.sql
+++ setup/modules/upgrade/sql/2.6.0-rc1/2.5.71.sql
@@ -15,13 +15,13 @@ ALTER TABLE {PREFIX}mod_contact
 
 
 ALTER TABLE {PREFIX}mod_media_cats
-	CHANGE clean_title identifier VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-	DROP INDEX clean_title,
+	CHANGE clean_name identifier VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	DROP INDEX clean_name,
 	ADD UNIQUE identifier ( identifier );
 
 ALTER TABLE {PREFIX}mod_media_items
-	CHANGE clean_title identifier VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-	DROP INDEX clean_title,
+	CHANGE clean_name identifier VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	DROP INDEX clean_name,
 	ADD UNIQUE identifier ( identifier );
 
 
