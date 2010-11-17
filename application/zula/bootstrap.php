399df752547ccecb7fd8b67deccb340653a3fa0d
Alex Cartwright <alex@tangocms.org>
2010-11-17 01:47:02 +0000

    'SET NAMES' query now only run when using 'mysql'

--- application/zula/bootstrap.php
+++ application/zula/bootstrap.php
@@ -45,7 +45,10 @@
 								  $sql['user'], $sql['pass'], $sql['port'], $sqlDriverOpts );
 		Registry::register( 'sql', $sqlConnection );
 		$sqlConnection->setPrefix( $sql['prefix'] );
-		$sqlConnection->query( 'SET NAMES "utf8"' ); # Use UTF-8 character set for the connection
+		if ( $sql['type'] == 'mysql' ) {
+			# Use UTF-8 character set for the connection
+			$sqlConnection->query( 'SET NAMES "utf8"' );
+		}
 		unset( $sqlConnection );
 		// Attempt to load the SQL configuration details
 		$configSql = new Config_sql;
