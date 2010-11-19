60a9f9e14c8aa21716117f947e4000a1628da25f
Robert Clipsham <robert@tangocms.org>
2010-11-19 19:45:17 +0000

    Quick hack for the article module for 1 indexed arrays PDO expects

--- application/modules/article/models/article.php
+++ application/modules/article/models/article.php
@@ -102,7 +102,7 @@
 				// Prepare and execute query
 				$result = $query->build();
 				$pdoSt = $this->_sql->prepare( $result[0] );
-				foreach( $result[1] as $ident=>$val ) {	 	
+				foreach( array_unshift($result[1], null) as $ident=>$val ) {
 					$pdoSt->bindValue( $ident, (int) $val, PDO::PARAM_INT );
 				}
 				$pdoSt->execute();
@@ -117,7 +117,7 @@
 				if ( $articles == false ) {
 					$result = $query->build();
 					$pdoSt = $this->_sql->prepare( $result[0] );
-					foreach( $result[1] as $ident=>$val ) {	 	
+					foreach( array_unshift($result[1], null) as $ident=>$val ) {
 						$pdoSt->bindValue( $ident, (int) $val, PDO::PARAM_INT );
 					}
 					$pdoSt->execute();
